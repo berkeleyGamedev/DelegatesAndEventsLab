ff61de5f79f399b2057024c5bbe6710c9ec73951
unknown <sabrina.wong@berkeley.edu>
2019-01-01 19:44:13 -0800

    init commit

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Bullet : MonoBehaviour
+{
+    #region Delegates and Events
+    // TODO: CODE HERE
+    public delegate void EnemyHitDelegate();
+    public static event EnemyHitDelegate EnemyDestroyedEvent;
+
+    #endregion
+
+    #region Collision Methods
+    private void OnParticleCollision(GameObject other)
+   {
+        if (other.CompareTag("Enemy") && EnemyDestroyedEvent != null)
+        {
+            EnemyDestroyedEvent();
+            other.SetActive(false);
+        }
+
+    }
+    #endregion
+
+    #region Unity Misc
+    private void OnParticleSystemStopped()
+   {
+      gameObject.SetActive(false);
+   }
+   #endregion
+}
