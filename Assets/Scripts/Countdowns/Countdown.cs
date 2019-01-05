00a81da5d3ed3fcac48dee81654c15c850555c82
unknown <sabrina.wong@berkeley.edu>
2019-01-04 17:02:10 -0800

    revised for s19

--- Assets/Scripts/Countdowns/Countdown.cs
+++ Assets/Scripts/Countdowns/Countdown.cs
@@ -8,58 +8,52 @@ public class Countdown : MonoBehaviour
 {
     #region Delegates and Events
     // TODO: CODE HERE
-    public delegate void StartGameDelegate();
-    public static event StartGameDelegate StartGameEvent;
-
     #endregion
 
     #region Editor Variables
     [SerializeField]
-   [Tooltip("How long before the game officially starts after pressing play.")]
-   private int m_Start = 3;
-   #endregion
+    [Tooltip("How long before the game officially starts after pressing play.")]
+    private int m_Start = 3;
+    #endregion
 
-   #region Cached Components
-   private Text m_Text;
-   #endregion
+    #region Cached Components
+    private Text m_Text;
+    #endregion
 
-   #region First Time Initialization and Set Up
-   private void Awake()
-   {
-      m_Text = GetComponent<Text>();
-   }
-   #endregion
+    #region First Time Initialization and Set Up
+    private void Awake()
+    {
+        m_Text = GetComponent<Text>();
+    }
+    #endregion
 
-   #region OnEnable, Set Ups, and Resetters
-   private void OnEnable()
-   {
+    #region OnEnable, Set Ups, and Resetters
+    private void OnEnable()
+    {
         StartCoroutine(BeginCountdown());
-        if (StartGameEvent != null)
-            StartGameEvent();
-
     }
     #endregion
 
     #region OnDisable and Other Enders
     private void OnDisable()
-   {
-      // TODO: CODE HERE
-   }
-   #endregion
+    {
+        // TODO: CODE HERE
+    }
+    #endregion
 
-   #region Countdown Methods
-   private IEnumerator BeginCountdown()
-   {
-      for (int i = m_Start; i > 0; i--)
-      {
-         m_Text.text = i.ToString();
-         yield return new WaitForSeconds(1);
-      }
+    #region Countdown Methods
+    private IEnumerator BeginCountdown()
+    {
+        for (int i = m_Start; i > 0; i--)
+        {
+            m_Text.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
 
-      m_Text.text = "GO!";
-      yield return new WaitForSeconds(0.5f);
-      
-      gameObject.SetActive(false);
-   }
-   #endregion
+        m_Text.text = "GO!";
+        yield return new WaitForSeconds(0.5f);
+
+        gameObject.SetActive(false);
+    }
+    #endregion
 }
